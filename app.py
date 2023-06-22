8dded99de00a0acb244d2fc575633e34b0cf36a1
Abdullah <abdullah.khondoker@g.bracu.ac.bd>
2023-06-22 06:37:27 +0600

    last night

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,session, render_template, request, redirect
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = "mongodb+srv://abdullah201:saveyourgrades@abdullah1065.hhjja0s.mongodb.net/uSavior"
+db = PyMongo(app).db
 
 @app.route("/home")
 @app.route("/")
@@ -11,6 +13,7 @@ def home():
 
 @app.route("/student_dashboard")
 def student_dashboard():
+    db.student.insert_one({'abdullah':2})
     return render_template('student_dashboard.html')
 
 
