4316b47e7987f0b58d7fb13b9fae4b71c415b005
Abdullah <abdullah.khondoker@g.bracu.ac.bd>
2023-06-22 15:18:07 +0600

    last night

--- app.py
+++ app.py
@@ -10,12 +10,34 @@ db = PyMongo(app).db
 def home():
     return render_template('home.html')
 
+@app.route("/student_login")
+def student_login():
+    return render_template('student_login.html')
+
+@app.route("/student_registration")
+def student_registration():
+    return render_template('student_registration.html')
+
+@app.route("/instructor_login")
+def instructor_login():
+    return render_template('instructor_login.html')
+
+@app.route("/instructor_registration")
+def instructor_registration():
+    return render_template('instructor_registration.html')
+
+@app.route("/forgotten_password")
+def forgotten_password():
+    return render_template('forgotten_password.html')
 
 @app.route("/student_dashboard")
 def student_dashboard():
-    db.student.insert_one({'abdullah':2})
+    #db.student.insert_one({'abdullah':2})
     return render_template('student_dashboard.html')
 
+@app.route("/instructor_dashboard")
+def instructor_dashboard():
+    return render_template('instructor_dashboard.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
