7e5a2c355c13499bf076df8fe89977e1879ff1f4
Abdullah <abdullah.khondoker@g.bracu.ac.bd>
2023-06-22 06:15:26 +0600

    last night

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask
+from flask import Flask,session, render_template, request, redirect
 
 app = Flask(__name__)
 
-print('uSavior')
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/student_dashboard")
+def student_dashboard():
+    return render_template('student_dashboard.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
